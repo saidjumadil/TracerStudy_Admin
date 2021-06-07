ffe66f0d12f169b6255bae1b3948240003db01c4
aqilfiqran <aqilfiqran19@gmail.com>
2021-06-07 21:14:25 +0700

    ✅ role permission and certification https

--- app/Middleware/D3/D3Auth.ts
+++ app/Middleware/D3/D3Auth.ts
@@ -4,7 +4,7 @@ const routeName: string = 'pasca.s2.'
 export default class D3Auth {
   public async handle({ request, response, auth }: HttpContextContract, next: () => Promise<void>) {
     await auth.authenticate()
-    // console.log(auth)
+
     if (auth.user?.permission_d3 === 0) {
       return response.redirect().toRoute('admin.' + routeName + 'index')
     }
