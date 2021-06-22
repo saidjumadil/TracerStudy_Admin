661b3dc1c777b4c61054d664615469520861f312
aqilfiqran <aqilfiqran19@gmail.com>
2021-06-22 21:06:20 +0700

    💄 fix tanggal isi ui

--- public/assets/js/custom/custom-datatable.js
+++ public/assets/js/custom/custom-datatable.js
@@ -41,7 +41,7 @@ const drawData = (data, csrf) => {
         <td> <label class="checkbox"> <input type="checkbox" data-id='${index}' value='${
       row.nim
     }' ${row.monitoring_3 ? 'checked' : ''} name="monitoring_3[]"> <span></span> </label> </td>
-        <td>${row.tanggal_isi || '-'}</td>
+        <td>${getTimeFormat(row.tanggal_isi) || '-'}</td>
         <td>
           ${
             row.username
@@ -105,4 +105,41 @@ const Status = (type, message) => {
       <span class="label label-lg font-weight-bold label-light-${type} label-inline">${message}</span>
     </span>
   `
+}
+
+const getTimeFormat = (value) => {
+  if (!value) return '-'
+  const months = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December',
+  }
+  const days = {
+    0: 'Minggu',
+    1: 'Senin',
+    2: 'Selasa',
+    3: 'Rabu',
+    4: 'Kamis',
+    5: "Jum'at",
+    6: 'Sabtu',
+  }
+  const waktu = new Date(value)
+  const bulan = waktu.getMonth()
+  const tahun = waktu.getFullYear()
+  const hari = waktu.getDay()
+  const date = waktu.getDate()
+  const jam = waktu.getHours()
+  const menit = waktu.getMinutes()
+  return ` ${days[hari]}, ${String(date).padStart(2, '0')} ${
+    months[bulan]
+  } ${tahun}. Pukul ${String(jam).padStart(2, '0')}:${String(menit).padStart(2, '0')} WIB`
 }
