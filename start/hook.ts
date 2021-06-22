7d83ea9abd175137ad40db5d81f364de8416f111
aqilfiqran <aqilfiqran19@gmail.com>
2021-06-22 20:35:28 +0700

    ✅ new hook and sasaran handle

--- start/hook.ts
+++ start/hook.ts
@@ -130,6 +130,7 @@ View.global('conString', function (value1: string, value2: string) {
 })
 
 View.global('getTitleSasaran', function (sasaran: string) {
+  if (!sasaran) return ''
   let tahun = sasaran.toString().substring(0, 4)
   let periode = sasaran.toString().substring(4, 5)
 
@@ -225,4 +226,41 @@ View.global('getPeriodeSasaran', function (daftarSasaran: any) {
     choice.push({ id: sasaran.tahun.substr(4, 5), nama: periodeString })
   })
   return choice
+})
+
+View.global('getTimeFormatStep', function (value: string) {
+  if (!value) return ''
+  const months = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December',
+  }
+  const days = {
+    0: 'Minggu',
+    1: 'Senin',
+    2: 'Selasa',
+    3: 'Rabu',
+    4: 'Kamis',
+    5: "Jum'at",
+    6: 'Sabtu',
+  }
+  const waktu = new Date(value)
+  const bulan = waktu.getMonth()
+  const tahun = waktu.getFullYear()
+  const hari = waktu.getDay()
+  const date = waktu.getDate()
+  const jam = waktu.getHours()
+  const menit = waktu.getMinutes()
+  return ` ${
+    days[hari]
+  }, ${String(date).padStart(2, '0')} ${months[bulan]} ${tahun} <br> ${String(jam).padStart(2, '0')}:${String(menit).padStart(2, '0')} WIB`
 })
