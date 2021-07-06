fe826c27787288d0c8d260e4a2a25885b0ec6580
aqilfiqran <aqilfiqran19@gmail.com>
2021-07-06 16:21:14 +0700

    ✅ add crt

--- build/server.js
+++ build/server.js
@@ -12,7 +12,7 @@ const https_1 = __importDefault(require("https"));
 source_map_support_1.default.install({ handleUncaughtExceptions: false });
 const options = {
     key: fs_1.default.readFileSync(path_1.default.join(__dirname, './ssl_certificate/unsyiah.ac.id.key')),
-    cert: fs_1.default.readFileSync(path_1.default.join(__dirname, './ssl_certificate/unsyiah.ac.id.pem')),
+    cert: fs_1.default.readFileSync(path_1.default.join(__dirname, './ssl_certificate/unsyiah.ac.id.crt')),
 };
 new standalone_1.Ignitor(__dirname).httpServer().start((handle) => {
     return https_1.default.createServer(options, handle);
